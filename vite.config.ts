3309dd720d552698cbf0c2e30ea8bab2a3b3a58f
kelzerock <kelzzzer@gmail.com>
2025-07-15 13:58:17 +0300

    feat: set proxy configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,16 @@ import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://codelang.vercel.app',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
 });
